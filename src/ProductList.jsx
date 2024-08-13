4f09d602c60324b1b4cd49bacd8d4bf529adfdae
marianoluiz <kmarianu@gmail.com>
2024-08-13 10:23:51 -0400

    task 2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -247,10 +247,6 @@ const handlePlantsClick = (e) => {
     setShowCart(false);
   };
 
-
-
-
-
     return (
         <div>
             {/* nav bar */}
@@ -270,7 +266,7 @@ const handlePlantsClick = (e) => {
               
             </div>
             <div style={styleObjUl}>
-                <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                <div> <a href="#" onClick={(e)=>  handlePlantsClick(e)} style={styleA}>Plants</a></div>
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
         </div>
@@ -278,11 +274,28 @@ const handlePlantsClick = (e) => {
         {!showCart? (
         <div className="product-grid">
 
-            {/* map like (state ) */}
+            {/* map param: (state element in the array, the index ) */}
             {plantsArray.map((category, index) => (
+
+                /* categories list*/
                 <div key={index}>
+
+                    <h1><div>{category.category}</div></h1>
+
                     <div className='product-list'>
-                        {category.plants.map((plant, plantIndex))}
+                    {category.plants.map((plant, plantIndex) => (
+                    /* products list */
+                    <div className="product-card" key={plantIndex}>
+
+                        <img className='product-image' src={plant.image} alt={plant.name} />
+
+                        <div className='product-title'>{plant.name}</div>
+
+                        {/*Similarly like the above plant.name show other details like description and cost*/}
+
+                        <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                    </div>
+                        ))}    
                     </div>
                 </div>
             ))}
