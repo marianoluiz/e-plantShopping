2334b718dc4cfa463b6bc0d41f7c0ef1ef2ea8f7
marianoluiz <kmarianu@gmail.com>
2024-08-14 05:32:49 -0400

    fixed cart increment, decrement, delete

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
  
 const CartItem = ({ onContinueShopping, totalCartQuantity }) => {
@@ -21,33 +21,25 @@ const CartItem = ({ onContinueShopping, totalCartQuantity }) => {
     onContinueShopping();
   };
 
-
-
   const handleIncrement = (item) => {
     
-    if(item) {
-        item.quantity++;
-        totalCartQuantity++;
-    }
-
-    dispatch(updateQuantity(item));
+    if (item) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+      }
   };
 
   const handleDecrement = (item) => {
-    if(item.quantity > 0) {
-        
-        item.quantity--;
-        totalCartQuantity--;
-
-        dispatch(updateQuantity(item));
-    } else {
-        dispatch(removeItem(item));
-    }
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+      } else {
+        dispatch(removeItem(item))
+      }
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem(item));
+    dispatch(removeItem(item.name));
   };
+
 
 
 
@@ -81,9 +73,14 @@ const CartItem = ({ onContinueShopping, totalCartQuantity }) => {
               <div className="cart-item-name">{item.name}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <button className="cart-item-button
+
+                cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+
                 <span className="cart-item-quantity-value">{item.quantity}</span>
+
                 <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
               <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
