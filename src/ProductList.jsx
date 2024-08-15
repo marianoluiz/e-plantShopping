1986afa4275596b0081b599394dabcad885fcf06
marianoluiz <kmarianu@gmail.com>
2024-08-14 23:10:15 -0400

    finished all functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -334,7 +334,7 @@ function ProductList() {
                 /* categories list*/
                 <div key={index}>
 
-                    <h1><div>{category.category}</div></h1>
+                    <h1><div className="product-category">{category.category}</div></h1>
 
                     <div className='product-list'>
                     {category.plants.map((plant, plantIndex) => {
