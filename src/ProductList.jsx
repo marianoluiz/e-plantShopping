65d33833d85dbf35953ba8cdaff5916cd7739afd
Mariano Luiz King <kmarianu@gmail.com>
2024-08-15 17:29:17 +0800

    baseUrl fix

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -289,11 +289,11 @@ function ProductList() {
                <div className="luxury">
                 
                 {/* logo */}
-                <a href={'${baseUrl}'} style={{textDecoration:'none'}}> 
+                <a href={`${baseUrl}`} style={{textDecoration:'none'}}> 
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt=""/></a>
 
                {/* home */}
-               <a href={'${baseUrl}'} style={{textDecoration:'none'}}>
+               <a href={`${baseUrl}`} style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
