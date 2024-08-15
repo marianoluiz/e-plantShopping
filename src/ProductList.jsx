3f814342994b1970bc775d33c709af5315235dc6
Mariano Luiz King <kmarianu@gmail.com>
2024-08-15 17:19:43 +0800

    modified relative path at productlist.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -286,11 +286,11 @@ function ProductList() {
                <div className="luxury">
                 
                 {/* logo */}
-                <a href="/" style={{textDecoration:'none'}}> 
+                <a href="./" style={{textDecoration:'none'}}> 
                <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt=""/></a>
 
                {/* home */}
-               <a href="/" style={{textDecoration:'none'}}>
+               <a href="./" style={{textDecoration:'none'}}>
                         <div>
                     <h3 style={{color:'white'}}>Paradise Nursery</h3>
                     <i style={{color:'white'}}>Where Green Meets Serenity</i>
