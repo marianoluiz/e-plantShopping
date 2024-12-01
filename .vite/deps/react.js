332f082206e5e854a11f47ed49cd3ac584ad81b0
marianoluiz <kmarianu@gmail.com>
2024-12-01 22:45:53 +0800

    fixed cart number bug and made image to local

---
+++ .vite/deps/react.js
@@ -0,0 +1,6 @@
+import {
+  require_react
+} from "./chunk-4EE4R5TB.js";
+import "./chunk-LNEMQRCO.js";
+export default require_react();
+//# sourceMappingURL=react.js.map
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
+import logoSrc from './assets/eco-logo.webp';
 function ProductList() {
 
     const baseUrl = import.meta.env.BASE_URL;
@@ -281,6 +282,8 @@ function ProductList() {
         dispatch(addItem(plant));
     }
 
+    
+
     return (
         <div>
             {/* nav bar */}
@@ -290,7 +293,7 @@ function ProductList() {
                 
                 {/* logo */}
                 <a href={`${baseUrl}`} style={{textDecoration:'none'}}> 
-               <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt=""/></a>
+                <img src={logoSrc} alt="Logo" /></a>
 
                {/* home */}
                <a href={`${baseUrl}`} style={{textDecoration:'none'}}>
@@ -306,10 +309,14 @@ function ProductList() {
                 <div> <a href="#" onClick={(e)=>  handlePlantsClick(e)} style={styleA}>Plants</a></div>
 
                 {/* this opens modal because u prevent default changing page */}
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
-                {/*  cart qty */}
-                <span className="item-count" style={itemCountStyle}>{cartQuantity}</span>
-                </h1></a>
+                <div> 
+                    <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <div className='cart' style={{position: "relative"}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>
+                            {/*  cart qty */}
+                        <span className="item-count" style={itemCountStyle}>{cartQuantity}</span>
+                        </div>
+                    </a>
                 </div>
 
             </div>
